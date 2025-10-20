1166ab1a7e45556cc6c2729a3e129925f59616cc
汪明杰 <1209497940@qq.com>
2025-10-20 11:10:11 +0800

    update

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/emoji-match-game' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/resume/' : '',
+}
+
+module.exports = nextConfig
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/emoji-match-game' : '',
+
 }
 
 export default nextConfig
